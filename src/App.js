948b8ac98e577738444ac3df5d9cb0897cab257c
Prashant Bhadula <prashant.bhadula19@gmail.com>
2024-03-26 11:23:11 +0530

    signIn/SIgnUp form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import Body from "./components/Body";
 
-import './App.css';
 
 export default function App() {
   return (
-    <div className="bg-blue-500 text-white p-4">
-      Hello, Tailwind CSS!
+    <div >
+      <Body />
     </div>
   );
 }
